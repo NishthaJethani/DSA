c5a0205276bfb0477ecbff2ba3b438d21eb122a5
Nishtha <95950571+NishthaJethani@users.noreply.github.com>
2023-06-03 12:12:59 +0530

    Update bfs.cpp

--- DSA Lab/bfs.cpp
+++ DSA Lab/bfs.cpp
@@ -1,56 +1,69 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-#define n 6
 
 class city
 {
 	string name;
 	int cost;
 	city * next;
-	
+
 	public:
-	city()
-	{
-		name="";
-		cost=0;
-		next=NULL;
-	}
-	city(string name, int cost)
-	{
-		this->name=name;
-		this->cost=cost;
-		next=NULL;
-	}
-    friend class graph;
+	
+		city()
+		{
+			name="";
+			cost=0;
+			next=NULL;
+		}
+
+		city(string name, int cost)
+		{
+			this->name=name;
+			this->cost=cost;
+			next=NULL;
+		}
+
+		friend class graph;
 };
+
 
 class queue
 {
 	int f, r;
-	string que[10];
-	public:
+	string *que;
+    int n;
 
-		queue()
+
+	public:
+	
+        queue()
+        {
+
+        }
+
+		queue(int num)
 		{
+            this->n=num;
+            que=new string[n];
 			f=r=-1;
 		}
-		
+	
 		bool qFull()
 		{
-			if(r==10-1)
+			if(r==n-1)
 				return true;
 			else
 				return false;
 		}
-		
+	
 		bool qEmpty()
 		{
-			if(f==-1 && r==-1)
+			if(f==-1 || f>r)
 				return true;
 			else
 				return false;
 		}
-		
+	
 		void insertQ(string city)
 		{
 			if(qFull())
@@ -62,20 +75,16 @@ class queue
 			}
 			else
 			{
-				que[r++]=city;
+				r++;
+				que[r]=city;
 			}
 		}
-		
+	
 		string deleteQ()
 		{
 			string city;
 			if(qEmpty())
-				cout<<"Queue is empty";
-			else if (f==r)
-			{
-				city=que[f];
-				r=f=-1;
-			}
+				return "Queue is empty";		
 			else
 			{
 				city=que[f];
@@ -83,7 +92,7 @@ class queue
 			}
 			return city;
 		}
-		
+	
 		bool inQueue(string city)
 		{
 			if(qEmpty())
@@ -97,32 +106,39 @@ class queue
 				}
 				return 0;
 			}
-		}
-			
+		}			
+	
 	friend class graph;
 };
-			
 
 class graph
 {
-	city *network[n];
-	queue q;
-	string visited[n];
-	int count=0;
+    city **network;
+    queue que;
+    string *visited;
+    int count=0;
+    int n;
 
-	public:
-		graph()
-		{
-			for(int i=0; i<n; i++)
-				network[i]=NULL;
-			createNetwork();
-		}
-		
-		void createNetwork()
-		{
-			int total_flights, time;
-			string city1, city2;
-			cout<<"Enter cities to connect:\n";
+    public:
+        graph()
+        {
+            cout<<"Enter number of cities: ";
+            cin>>n;
+            network=new city*[n];
+            queue que(n);
+            visited=new string[n];
+
+            for(int i=0; i<n; i++)
+                network[i]=NULL;
+
+            createnetwork();
+        }
+        
+        void createnetwork()
+        {
+            string city1, city2;
+            int total_flights, time;
+            cout<<"Enter cities to connect:\n";
 			for(int i=0; i<n; i++)
 			{
 				network[i]= new city();
@@ -130,7 +146,7 @@ class graph
 				cin>>network[i]->name;
 			}
 
-			cout<<"Enter total number of flights in network: ";
+            cout<<"Enter total number of flights in network: ";
 			cin>>total_flights;
 			for(int i=0; i<total_flights; i++)
 			{
@@ -143,20 +159,21 @@ class graph
 				insert(city1, city2, time);
 				insert(city2, city1, time);
 			}
-		}
+        }
 
-		void insert(string city1, string city2, int time)
-		{
-			city *dest = new city(city2, time);
-			
+        void insert(string city1, string city2, int time)
+        {
+            city *dest = new city(city2, time);
+
 			int index=getindex(city1);
 			city *source=network[index];
+			
 			while(source->next!=NULL)
 				source=source->next;
 			source->next=dest;
-		}
-		
-		int getindex(string city)
+        }
+
+        int getindex(string city)
 		{
 			for(int i=0; i<n; i++)
 			{
@@ -166,7 +183,7 @@ class graph
 			return -1;
 		}
 
-		void display()
+        void display()
 		{
 			for(int i=0; i<n; i++)
 			{
@@ -178,31 +195,28 @@ class graph
 				}
 				cout<<"NULL"<<endl;
 			}
+        }
 
-		}
-	
-		void bfs(string source)
+        void bfs(string source)
 		{
-			q.insertQ(source);
-			
+			que.insertQ(source);	
 			city *head;
 			string visited_city;
 
-			while(!q.qEmpty())
+			while(!que.qEmpty())
 			{
-				visited_city=q.deleteQ();
+				visited_city=que.deleteQ();
 				cout<<visited_city<<"\t";
 				visited[count]=visited_city;
 				count++;
 
 				int index=getindex(visited_city);
 				head=network[index];
-
 				while(head->next!=NULL)
 				{
 					head=head->next;
-					if((!q.inQueue(head->name)) && (!isVisited(head->name)))
-						q.insertQ(head->name);
+					if((!que.inQueue(head->name)) && (!isVisited(head->name)))
+						que.insertQ(head->name);
 				}
 			}
 		}
@@ -220,11 +234,8 @@ class graph
 
 int main()
 {
-	int choice;
-
 	graph g;
 	g.display();
 	cout<<"BFS: ";
-	g.bfs("a");
-	
+	g.bfs("a");	
 }
