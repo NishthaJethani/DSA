80ccf717d2e66b7dbac66a82cb0afbe72b99c34d
Nishtha <95950571+NishthaJethani@users.noreply.github.com>
2023-06-03 11:50:41 +0530

    Update prims.cpp

--- DSA Lab/prims.cpp
+++ DSA Lab/prims.cpp
@@ -1,39 +1,58 @@
 #include <iostream>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define n 9
 
 class graph
 {
-    int G[n][n]={
-	{0, 4, 0, 0, 0, 0, 0, 8, 0},
-	{4, 0, 8, 0, 0, 0, 0, 11, 0},
-	{0, 8, 0, 7, 0, 4, 0, 0, 2},
-	{0, 0, 7, 0, 9, 14, 0, 0, 0},
-	{0, 0, 0, 9, 0, 10, 0, 0, 0},
-    {0, 0, 4, 14, 10, 0, 2, 0, 0},
-	{0, 0, 0, 0, 0, 2, 0, 1, 6},
-	{8, 11, 0, 0, 0, 0, 1, 0, 7},
-    {0, 0, 2, 0, 0, 0, 6, 7, 0},
-	};
-
-    int visited[n];
+    int **adjacency;
+    int n;
+    bool *visited;
 
     public:
-        
         graph()
         {
-            for (int i=0; i<n; i++)
+            cout<<"Enter number of nodes: ";
+            cin>>n;
+            adjacency=new int*[n];
+            for(int i=0; i<n; i++)
+                adjacency[i]=new int[n];
+
+            for(int i=0; i<n; i++)
+            {
+                for(int j=i; j<n; j++)
+                {
+                    cout<<"Enter cost of edge between "<<i<<" and "<<j<<": ";
+                    cin>>adjacency[i][j];
+                    adjacency[j][i]=adjacency[i][j];
+                }
+            }
+
+            visited=new bool[n];
+            for(int i=0; i<n; i++)
             {
                 visited[i]=false;
             }
         }
 
+        void display(string prompt)
+        {
+            cout<<prompt<<endl;
+            for(int i=0; i<n; i++)
+            {
+                for(int j=0; j<n; j++)
+                    cout<<adjacency[i][j]<<" ";
+                cout<<endl;
+            }
+        }
+
         void prims()
         {
-            int min, x, y;
-            int edges=0, cost=0;
+            int edges=0;
+            int min;
+            int x, y;
+            int cost=0;
             visited[0]=true;
+
             while(edges<n-1)
             {
                 min=INT_MAX;
@@ -43,29 +62,31 @@ class graph
                     {
                         for(int j=0; j<n; j++)
                         {
-                            if(!visited[j] && G[i][j])
+                            if(!visited[j] && adjacency[i][j])
                             {
-                                if(min>G[i][j])
+                                if(adjacency[i][j]<min)
                                 {
-                                    min=G[i][j];
                                     x=i;
                                     y=j;
+                                    min=adjacency[i][j];
                                 }
                             }
                         }
                     }
                 }
-                cout<<x<<" --> "<<y<<": "<<G[x][y]<<endl;
-                cost+=G[x][y];
+                cout<<x<<"--->"<<y<<": "<<min<<endl;
+                cost+=min;
                 visited[y]=true;
                 edges++;
             }
             cout<<"Minimum Cost: "<<cost;
         }
+
 };
 
 int main()
 {
-	graph g;
+    graph g;
+    g.display("Entered Matrix:");
     g.prims();
 }
